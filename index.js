3bc8cc4f739601a1015463345e0d2f3ac1805fff
James Parrish <jthomasparrishjr@gmail.com>
2022-06-28 22:15:06 -0500

    commit tree 7314b17ba15e1d2396402d472ed2c23e2eb01316

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // for (let i = 0; i < array.length; i++){
+  //   const complement = target - array[i];
+  //   for (let j = i + 1; j < array.length; j++){
+  //       return (array[j] === complement) ? true : false 
+  //       }
+  // }
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
